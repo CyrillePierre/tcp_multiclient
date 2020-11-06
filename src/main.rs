aecff925b2855e188669108f4f5791dfa6caa559
Cyrille <cypierre58@gmail.com>
2020-11-06 01:48:51 +0100

    Iterates on ports

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,32 @@
 use std::net::{TcpListener, TcpStream};
 
 fn main() {
-    let mut args = std::env::args();
+    let mut args = std::env::args().peekable();
     let prog_name = args.next().unwrap_or_else(|| help("./<name_of_executable>"));
-    let port = args.next().unwrap_or_else(|| help(&prog_name));
+    let ip = "[::]";
+
+    // check if there is an argument
+    args.peek().unwrap_or_else(|| help(&prog_name));
 
     // generate TcpListeners from the port
-    let servers: Vec<TcpListener> = ["[::1]", "0.0.0.0"].iter()
-        .filter_map(|ip| 
-            match TcpListener::bind(format!("{}:{}", ip, port)) {
+    //let servers: Vec<TcpListener> = ["[::1]", "0.0.0.0", "127.0.0.1"].iter()
+    let servers: Vec<TcpListener> = args
+        .filter_map(|port| {
+            let addr = format!("{}:{}", ip, port);
+            match TcpListener::bind(&addr) {
                 Ok(tl) => { println!("Binding {}", tl.local_addr().unwrap()); Some(tl) },
-                Err(e) => { eprintln!("Failed to bind {}: {}", ip, e); None }
-            })
+                Err(e) => { eprintln!("Failed to bind {}: {}", addr, e); None }
+            }
+        })
         .collect();
     if servers.is_empty() {
-        eprintln!("No IPv4 or IPv6 bind available for TCP port '{}'.", port);
+        eprintln!("No IPv4 or IPv6 bind available.");
         std::process::exit(2);
     }
     println!("{:?}", servers);
 }
 
 fn help(arg0: &str) -> ! {
-    eprintln!("Syntax: {} <port>", arg0);
+    eprintln!("Syntax: {} <port> [<port2> ...]", arg0);
     std::process::exit(1);
 }
